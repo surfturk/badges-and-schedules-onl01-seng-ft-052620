71f9317bc89953f868072fda8afb1fa59a5df6e0
Berkley Brown <berkleydeane@gmail.com>
2020-05-28 19:51:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+  end  
+  
+
+   def batch_badge_creator(names)
+     names.map do |name|
+       badge_maker(name)
+   end
+  end
+ 
+
+ def assign_rooms(attendees)
+   attendees.map.with_index(1) do |attendee, index|
+     "Hello, #{attendee}! You'll be assigned to room #{index}!"
+ end
+end
+
+def printer(names)
+ batch_badge_creator(names).each do |badge|
+   puts badge
+  end 
+  assign_rooms(attendees).each do |assigned_room|
+    puts assigned_room
+  end
+end
